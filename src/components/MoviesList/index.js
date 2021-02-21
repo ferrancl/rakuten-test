c5c25652c2ddab7d1b6514a4567a9a89f0395eac
Ferran Calvo <Ferran@MBP-de-Ferran.lan>
2021-02-21 18:07:43 +0100

    styles on notfound page

--- src/components/MoviesList/index.js
+++ src/components/MoviesList/index.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './styles.css'
 
 export const MoviesList = ({ onclick, movies }) => {
 
+    // const moviesCarousel = useRef()
+
     return (
         <div className="category__movies">
-            {movies?.map(({movieId, image}) => {
-                return <div className="category__movies-item" onClick={()=> onclick(movieId)} key={movieId}>
-                    <img src={image} alt=""/>
-                </div>
-            })}
+            {/* <div className="category__movies-left">
+                <i className="fas fa-angle-left" style={{color: 'white'}} onClick={()=> 
+                    moviesCarousel.current.scrollLeft -= moviesCarousel.current.offsetWidth}>
+                </i>
+            </div> */}
+
+                {movies?.map(({movieId, image}) => {
+                    return <div className="category__movies-item" onClick={()=> onclick(movieId)} key={movieId}>
+                        <img src={image} alt=""/>
+                        </div>
+                })}
+            {/* <div className="category__movies-right" onClick={()=> 
+                moviesCarousel.current.scrollLeft += moviesCarousel.current.offsetWidth}>
+                <i className="fas fa-angle-right" style={{color: 'white'}}></i>
+            </div> */}
         </div>
     )
 }
